99c69e4b0c00642b611bccfbdfe302154ccc594e
Alysson Bruno O. Santos <alyssonbrunooliveirasantos@gmail.com>
2025-04-13 19:18:13 -0300

    replace: update .ino files with English comments

--- arduino-projects/basics/projeto04_traffic_lights/projeto04_traffic_lights.ino
+++ arduino-projects/basics/projeto04_traffic_lights/projeto04_traffic_lights.ino
@@ -1,29 +1,32 @@
-int ledVerde = 8;
-int ledAmarelo = 9;
-int ledVermelho = 10;
+// Project 04: Traffic Light Simulation
+
+int ledVerde = 8;     // Green light (pin 8)
+int ledAmarelo = 9;   // Yellow light (pin 9)
+int ledVermelho = 10; // Red light (pin 10)
 
 void setup() {
+  // Define each LED pin as an output
   pinMode(ledVerde, OUTPUT);
   pinMode(ledAmarelo, OUTPUT);
   pinMode(ledVermelho, OUTPUT);
 }
 
 void loop() {
-  // Luz verde acesa (passar)
+  // Green light ON (cars go)
   digitalWrite(ledVerde, HIGH);
   digitalWrite(ledAmarelo, LOW);
   digitalWrite(ledVermelho, LOW);
-  delay(4000); // 4 segundos
+  delay(4000); // Wait 4 seconds
 
-  // Luz amarela acesa (atenção)
+  // Yellow light ON (warning)
   digitalWrite(ledVerde, LOW);
   digitalWrite(ledAmarelo, HIGH);
   digitalWrite(ledVermelho, LOW);
-  delay(2000); // 2 segundos
+  delay(2000); // Wait 2 seconds
 
-  // Luz vermelha acesa (pare)
+  // Red light ON (cars stop)
   digitalWrite(ledVerde, LOW);
   digitalWrite(ledAmarelo, LOW);
   digitalWrite(ledVermelho, HIGH);
-  delay(4000); // 4 segundos
+  delay(4000); // Wait 4 seconds
 }
