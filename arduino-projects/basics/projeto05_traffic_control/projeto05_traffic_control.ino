1d804a02d767cdd63f68ac8c52b33a9193f8a197
Alysson Bruno O. Santos <alyssonbrunooliveirasantos@gmail.com>
2025-04-13 19:18:58 -0300

    replace: update .ino files with English comments

--- arduino-projects/basics/projeto05_traffic_control/projeto05_traffic_control.ino
+++ arduino-projects/basics/projeto05_traffic_control/projeto05_traffic_control.ino
@@ -1,33 +1,38 @@
-// Definição dos pinos para os LEDs do semáforo de trânsito
-const int ledVerdeCarro = 10;
-const int ledAmareloCarro = 11;
-const int ledVermelhoCarro = 12;
+// Project 05: Traffic Light with Pedestrian Button
 
-// Definição dos pinos para os LEDs do semáforo de pedestres
-const int ledVerdePedestre = 8;
-const int ledVermelhoPedestre = 9;
+// Pin definitions for car traffic light LEDs
+const int ledVerdeCarro = 10;     // Green light for cars
+const int ledAmareloCarro = 11;   // Yellow light for cars
+const int ledVermelhoCarro = 12;  // Red light for cars
 
-// Pino do botão para pedestres
-const int BOTAO = 2;
+// Pin definitions for pedestrian LEDs
+const int ledVerdePedestre = 8;   // Green light for pedestrians
+const int ledVermelhoPedestre = 9; // Red light for pedestrians
 
-// Variáveis para controle do estado do botão
+// Pedestrian button pin
+const int BOTAO = 2; // Button to request pedestrian crossing
+
+// Variables to track button state
 int estadoBotao = 0;
 int ultimoEstadoBotao = LOW;
 
-// Tempos
-const long tempoVerdeCarro = 10000;     // 10 segundos
-const long tempoAmareloCarro = 3000;    // 3 segundos
-const long tempoVerdePedestre = 8000;   // 8 segundos
+// Timings (in milliseconds)
+const long tempoVerdeCarro = 10000;     // Green light duration for cars (10s)
+const long tempoAmareloCarro = 3000;    // Yellow light duration (3s)
+const long tempoVerdePedestre = 8000;   // Pedestrian crossing time (8s)
 
 void setup() {
+  // Set all LED pins as OUTPUT
   pinMode(ledVermelhoCarro, OUTPUT);
   pinMode(ledAmareloCarro, OUTPUT);
   pinMode(ledVerdeCarro, OUTPUT);
   pinMode(ledVermelhoPedestre, OUTPUT);
   pinMode(ledVerdePedestre, OUTPUT);
+
+  // Set button pin as INPUT
   pinMode(BOTAO, INPUT);
 
-  // Estado inicial
+  // Initial state: cars can go, pedestrians must wait
   digitalWrite(ledVerdeCarro, HIGH);
   digitalWrite(ledAmareloCarro, LOW);
   digitalWrite(ledVermelhoCarro, LOW);
@@ -37,35 +42,39 @@ void setup() {
 }
 
 void loop() {
+  // Read the current state of the button
   estadoBotao = digitalRead(BOTAO);
 
-  // Se botão for pressionado e ainda não estava pressionado antes
+  // If the button has just been pressed
   if (estadoBotao == HIGH && ultimoEstadoBotao == LOW) {
-    delay(50); // debounce
+    delay(50); // Debounce delay
     iniciarTravessiaPedestre();
   }
 
+  // Store current button state
   ultimoEstadoBotao = estadoBotao;
 }
 
 void iniciarTravessiaPedestre() {
-  // Mudar semáforo do carro para vermelho
+  // Switch car light to yellow
   digitalWrite(ledVerdeCarro, LOW);
   digitalWrite(ledAmareloCarro, HIGH);
   delay(tempoAmareloCarro);
 
+  // Stop cars with red light
   digitalWrite(ledAmareloCarro, LOW);
   digitalWrite(ledVermelhoCarro, HIGH);
 
-  // Libera pedestre
+  // Allow pedestrians to cross
   digitalWrite(ledVermelhoPedestre, LOW);
   digitalWrite(ledVerdePedestre, HIGH);
   delay(tempoVerdePedestre);
 
-  // Volta à condição inicial
+  // End pedestrian crossing, turn red for them
   digitalWrite(ledVerdePedestre, LOW);
   digitalWrite(ledVermelhoPedestre, HIGH);
 
+  // Return car light to green
   digitalWrite(ledVermelhoCarro, LOW);
   digitalWrite(ledVerdeCarro, HIGH);
 }
